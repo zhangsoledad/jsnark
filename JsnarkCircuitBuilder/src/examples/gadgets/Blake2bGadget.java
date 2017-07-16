02b627ceafa792d4407ce564056096494a7064b5
rink1969 <rink1969@cryptape.com>
2017-07-16 11:49:24 +0800

    fix iv

--- JsnarkCircuitBuilder/src/examples/gadgets/Blake2bGadget.java
+++ JsnarkCircuitBuilder/src/examples/gadgets/Blake2bGadget.java
@@ -13,8 +13,8 @@ import circuit.structure.WireArray;
 
 public class Blake2bGadget extends Gadget {
 
-	private static final long H[] = {0xcbbb9d5dc1059ed8L, 0x629a292a367cd507L, 0x9159015a3070dd17L, 0x152fecd8f70e5939L, 0x67332667ffc00b31L, 0x8eb44a8768581511L,
-			0xdb0c2e0d64f98fa7L, 0x47b5481dbefa4fa4L};
+	private static final long H[] = {0x6a09e667f3bcc908L, 0xbb67ae8584caa73bL, 0x3c6ef372fe94f82bL, 0xa54ff53a5f1d36f1L, 0x510e527fade682d1L, 0x9b05688c2b3e6c1fL,
+			0x1f83d9abfb41bd6bL, 0x5be0cd19137e2179L};
 
 	private static final int SIGMA[][] = {
 			{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
